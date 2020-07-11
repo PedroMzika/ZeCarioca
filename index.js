2ae74ad4d1f40a8f363af19fde44673b7f2c20cc
shaundeveloper <mastercanguro@hotmail.com>
2020-07-11 01:29:44 -0300

    Pai errou o index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { config } = require('dotenv');
 
 config(); // Run dotenv configuration.
 
-const Pereira = require('./src/Pereira');
+const Parrot = require('./src/Parrot');
 const client = new Pereira()
 
 client.initializeLoaders();
