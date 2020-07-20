c2c3bc0626859d966c838e3a233b7c27c7ab043e
shaundeveloper <mastercanguro@hotmail.com>
2020-07-20 19:37:26 -0300

    adicionando comando de delete, e arrumando o help

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -16,7 +16,6 @@ module.exports = class HelpCommand extends Command {
     const HelpEmbed = new ParrotEmbed(author)
       .setTitle(`Atualmente o bot contem: ${this.client.commands.size} comandos!`)
       .addField(`Comandos do Bot: ${this.client.commands.filter(filterCommands("Bot")).size}`, this.client.commands.filter(filterCommands("Bot")).map(c => `\`${c.name}\``).join(',\n'))
-			.addField(`Comandos de Desenvolvedores: ${this.client.commands.filter(filterCommands("Developer")).size}`, this.client.commands.filter(filterCommands("Developer")).map(c => `\`${c.name}\``).join(",\n"))
 			.addField(`Comandos de Informação: ${this.client.commands.filter(filterCommands("Info")).size}`, this.client.commands.filter(filterCommands("Info")).map(c => `\`${c.name}\``).join(",\n"))
 			.addField(`Comandos de Música: ${this.client.commands.filter(filterCommands("Musica")).size}`, this.client.commands.filter(filterCommands("Musica")).map(c => `\`${c.name}\``).join(",\n"))
     channel.send(HelpEmbed);
