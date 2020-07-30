594f503540befab34b5b19b0b2a866ff736c0e5b
shaundeveloper <mastercanguro@hotmail.com>
2020-07-30 18:37:33 -0300

    Desbugando os commits

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -15,9 +15,6 @@ module.exports = class StopCommand extends Command {
 	async run({ message, author, channel, member }) {
 		const player = this.client.music.players.get(message.guild.id);
 
-<<<<<<< HEAD
-		if (!player) return channel.send(new ParrotEmbed(author) .setDescription("⚠️ | Não há músicas tocando no momento!"));
-
 		if (player.voiceChannel !== member.voice.channel.id) return channel.send(new ParrotEmbed(author) .setDescription("⚠️ | Você não está no mesmo canal que eu!"));
 		
 		if (author.id == player.track.info.requester.id || author.id == player.dj.id) {
@@ -28,15 +25,3 @@ module.exports = class StopCommand extends Command {
 		}     
 	}
 };
-=======
-    if (player.voiceChannel !== member.voice.channel.id) return channel.send(new ParrotEmbed() .setDescription("⚠️ | Você não está no mesmo canal que eu!"));
-    
-    if (author.id == player.track.info.requester.id || author.id == player.dj.id) {
-      player.destroy();
-      channel.send(new ParrotEmbed() .setDescription("<:musicEject:708136949365473340> | Saindo do canal de voz."));
-    } else {
-      return channel.send(new ParrotEmbed() .setDescription("⚠️ | Você não é o DJ/requester deste(a) canal/música."));
-    }     
-  };
-}
->>>>>>> d7f4f68169faf2c0a1543ba7a31055b648382402
