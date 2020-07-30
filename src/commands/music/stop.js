a3140b58fa7bc4e9846fe8044891cdd6222ff1cd
ShaunDev <42876444+shaundeveloper@users.noreply.github.com>
2020-07-30 14:40:47 -0300

    Tentando arrumar um bug

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -15,8 +15,6 @@ module.exports = class StopCommand extends Command {
   async run({ message, author, client, channel, member }) {
     const player = this.client.music.players.get(message.guild.id);
 
-    if (!player) return channel.send(new ParrotEmbed() .setDescription("⚠️ | Não há músicas tocando no momento!"));
-
     if (player.voiceChannel !== member.voice.channel.id) return channel.send(new ParrotEmbed() .setDescription("⚠️ | Você não está no mesmo canal que eu!"));
     
     if (author.id == player.track.info.requester.id || author.id == player.dj.id) {
